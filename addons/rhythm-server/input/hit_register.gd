889fa70f2237b04e2cdbbe687bbfed1da21be8be
moondog <159832633+dog-on-moon@users.noreply.github.com>
2025-02-17 11:54:38 -0600

    hit register: rework a bit

--- addons/rhythm-server/input/hit_register.gd
+++ addons/rhythm-server/input/hit_register.gd
@@ -2,8 +2,13 @@ extends Node
 class_name HitRegister
 ## A class that handles hit registration with a HitListener.
 
+## The beat window to check for inputs within.
+## This should be kept as low as possible,
+## but should remain above the msec window.
+const BEAT_WINDOW := 2.0
+
 ## Emitted when a hit has been registered.
-signal hit_success(hit: Hit)
+signal hit_success(hit: Hit, offset: float)
 
 ## Emitted when a hit has been missed.
 signal hit_failure(hit: Hit)
@@ -15,8 +20,8 @@ signal hit_failure(hit: Hit)
 ## The name of the input action.
 @export var input_action := &"ui_accept"
 
-## The msec window to accept inputs within.
-@export var msec_window := 100
+## The second window to accept inputs within.
+@export var second_window := 0.1
 
 ## Whether or not hit registration is active.
 @export var active := true:
@@ -28,44 +33,42 @@ signal hit_failure(hit: Hit)
 @export var auto := false
 #endregion
 
-## Dictionaries for note input
+## Tracks all hits we're watching for.
 var hit_times := {}
-var last_press_msec := 0
 
 func _ready() -> void:
 	assert(track_name, "TrackName undefined")
-	RhythmServer.add_down_callback(self, track_name, hit_down, false)
+	RhythmServer.add_cue_callback(self, track_name, hit_cue, BEAT_WINDOW, false)
 
 ## Look out for any notes we've missed.
 func _process(delta: float) -> void:
-	for hit in hit_times:
-		if has_missed_hit(hit):
+	var t := RhythmServer.get_current_time(false)
+	for hit in hit_times.keys():
+		if has_missed_hit(hit, t):
 			mark_miss(hit)
 
 ## Handle receiving an input action.
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed(input_action):
-		last_press_msec = Time.get_ticks_msec()
+		var press_t := RhythmServer.get_current_time(false)
 		for hit in hit_times:
-			if has_hit_in_window(hit):
-				mark_hit(hit)
+			if has_hit_in_window(hit, press_t):
+				mark_hit(hit, press_t)
 				get_viewport().set_input_as_handled()
-				break
+				return
+		hit_failure.emit(null)
+		get_viewport().set_input_as_handled()
 
-## When the note is hit down, mark the time when it was to have been hit.
-func hit_down(hit: Hit):
-	hit_times[hit] = Time.get_ticks_msec() # TODO: make this more precise & exist on the hit object
-	if has_hit_in_window(hit):
-		mark_hit(hit)
+func hit_cue(hit: Hit):
+	hit_times[hit] = RhythmServer.convert_beat_to_time(hit.beat)
 
 ## Marks a hit as being successful.
-func mark_hit(hit: Hit):
-	hit_success.emit(hit)
+func mark_hit(hit: Hit, press_t: float):
+	hit_success.emit(hit, press_t - RhythmServer.convert_beat_to_time(hit.beat))
 	cleanup_hit(hit)
-	last_press_msec = 0
-	#var offset := (last_press_msec - last_hit_msec)
-	#var response := "late" if offset > 0 else "early"
-	#print('HIT! ({0}: {1})'.format([response, last_press_msec - last_hit_msec]))
+	
+	#var response := "late" if _offset > 0 else "early"
+	#print('HIT! ({0}: {1})'.format([response, _offset]))
 
 ## Marks a hit as being a miss.
 func mark_miss(hit: Hit):
@@ -76,9 +79,9 @@ func cleanup_hit(hit: Hit):
 	hit_times.erase(hit)
 
 ## Determines if we have successfully hit in the window.
-func has_hit_in_window(hit: Hit) -> bool:
-	return (abs(hit_times[hit] - last_press_msec) <= msec_window) or auto
+func has_hit_in_window(hit: Hit, press_t: float) -> bool:
+	return (abs(hit_times[hit] - press_t) <= second_window) or auto
 
 ## Determines if we've missed a hit.
-func has_missed_hit(hit: Hit) -> bool:
-	return (Time.get_ticks_msec() - hit_times[hit]) > msec_window
+func has_missed_hit(hit: Hit, current_t: float) -> bool:
+	return (current_t - hit_times[hit]) > second_window
