ae191b442bb604585c2d8806efe42dd770554f02
moondog <159832633+dog-on-moon@users.noreply.github.com>
2025-02-17 11:54:28 -0600

    note: add pitch scale conversion

--- addons/rhythm-server/data/note.gd
+++ addons/rhythm-server/data/note.gd
@@ -3,6 +3,8 @@ class_name Note
 ## A single music note.
 ## Holds pitch, octave, and (optional) playback effects.
 
+const TWELVTH_ROOT := pow(2.0, 1.0 / 12.0)
+
 ## An enum representing each pitch.
 ## Contains a null value for pitchless.
 enum Pitch {
@@ -23,8 +25,14 @@ enum Pitch {
 @export var volume := 1.0
 
 var total_pitch: int:
-	get:
-		return (octave * 12) + int(pitch) - 1
+	get: return get_total_pitch(pitch, octave)
+
+func get_pitch_scale(root_pitch := Pitch.C, root_octave := 3) -> float:
+	var dist := total_pitch - get_total_pitch(root_pitch, root_octave)
+	return pow(TWELVTH_ROOT, dist)
+
+static func get_total_pitch(_pitch: Pitch, _octave: int) -> int:
+	return (_octave * 12) + int(_pitch) - 1
 
 func _to_string() -> String:
 	return str(total_pitch)
